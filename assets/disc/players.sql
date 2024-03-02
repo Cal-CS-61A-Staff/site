72856242b659a2b5bdf08fa831f27d5974a115c4
John DeNero <denero@berkeley.edu>
2024-03-01 17:15:40 -0800

    ants

--- assets/disc/players.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE players AS
-    SELECT "Ryan Sanborn" AS name, "Stanford" AS team UNION
-    SELECT "Donald Stewart", "Stanford" UNION
-    SELECT "Cameron Scarlett", "Stanford" UNION
-    SELECT "Christopher Brown Jr.", "Cal" UNION
-    SELECT "Greg Thomas", "Cal" UNION
-    SELECT "Nikko Remigio", "Cal" UNION
-    SELECT "Chase Garbers", "Cal";
-
